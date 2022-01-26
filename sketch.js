655db38a701de048e96fe4660ffb40a9c5af298b
Leonardo B Brentano <95104325+leobbt@users.noreply.github.com>
2022-01-26 09:20:54 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var starImg,bgImg;
+var star, starBody;
+var fada, fadaimg;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+    starImg = loadImage("images/star.png");
+	bgImg = loadImage("images/starNight.png");
+    fadaimg = loadImage("images/fairy.png")
+}
+
+function setup() {
+    createCanvas(800, 750);
+
+    fada = createSprite(200,100);
+	fada.addImage(fadaimg);
+
+    star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+
+    engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
+
+}
